3412dbe0f680b13fcef1bb6bef64ec9237902b6d
Rebeljah <rebeljah@gmail.com>
2021-11-14 00:57:19 -0500

    Async tkinter skeleton

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from ui.root import WizWizardApp
-from backend.home import Home
+import tkinter as tk
+from tkinter import ttk
+import asyncio
+
+
+class AppRoot(tk.Tk):
+    def __init__(self, loop):
+        super().__init__()
+        self.protocol("WM_DELETE_WINDOW", self.close)
+        self.loop = loop
+
+        self.build()
+        self.loop.create_task(self.run())
+
+    def build(self):
+        pass
+
+    async def run(self):
+        while True:
+            self.update()
+            await asyncio.sleep(1/30)
+
+    def close(self):
+        self.loop.stop()
+        self.destroy()
 
 
 if __name__ == '__main__':
-    home = Home.from_save("0000000")
-    app = WizWizardApp(home)
-    app.run()
+    def main():
+        loop = asyncio.get_event_loop()
+        root = AppRoot(loop)
+        loop.run_forever()
+
+    main()
