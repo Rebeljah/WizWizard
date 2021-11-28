48495e8a6b01f41cb1860e517e5fefcafe7506e6
Jarrod <rebeljah@gmail.com>
2021-11-28 00:42:40 -0500

    Adjusted file tree. save data and iamges are now located in the data folder. Also added a settings.json for future use.

--- backend/home.py
+++ backend/home.py
@@ -81,14 +81,14 @@ class Home:
                     ]
                 }
 
-        filepath = os.path.join('save_data', f"{self.id}.json")
+        filepath = os.path.join('data', f"{self.id}.json")
         utils.save_dict_json(data, filepath, indent=4)
 
     @classmethod
     def from_save(cls, home_uid: str):
         """Load then parse home_model data from JSON and return a Home instance"""
 
-        filepath = os.path.join('save_data', f"{home_uid}.json")
+        filepath = os.path.join('data', 'homes', f"{home_uid}.json")
         home_data = utils.load_dict_json(filepath)
 
         # create Home
